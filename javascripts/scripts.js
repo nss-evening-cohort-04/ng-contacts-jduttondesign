b71d637977690de7bc3defc9781883ddb054dd37
Jennifer Dutton <jduttondesign@gmail.com>
2016-11-16 16:46:29 -0600

    initial commit

---
+++ javascripts/scripts.js
@@ -0,0 +1,9 @@
+main.js
+
+function PeopleController($scope) {
+	$scope.people[
+	{name:"Joe Doe", phone:6155551212, city:"Nashville"},
+	{name:"Jennifer", phone:6155551212, city:"Nashville"},
+	{name:"Pam", phone:6144881212, city:"Columbus"}
+	];
+}
